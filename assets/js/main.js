3283ad66194edfa4d5046d4dfa92d9d066a69e60
Seral Yilar <seral936@msn.com>
2019-11-07 14:07:38 +0100

    Changement du mot clé et de la reponse de retour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 // -----------J'INSTANCIE new SpeechRecognition
-const recognition = new SpeechRecognition
-// -----------JE RECUPERE LA DIV QUI CONTIENDRA LA TEXTE QU'IL VA ME RETOURNER 
-const transcription_element = document.getElementById('transcription')
+const recognition = new SpeechRecognition();
+// -----------JE RECUPERE LA DIV QUI CONTIENDRA LA TEXTE QU'IL VA ME RETOURNER
+const transcription_element = document.getElementById("transcription");
 
 // const btnStart = document.getElementById('start')
 // const btnStop = document.getElementById('end')
 
-// -----------JE CREER UNE BALISE P 
-let p = document.createElement('p')
-// -----------J'INSERE MA BALISE P DANS MA DIV 
-transcription_element.appendChild(p)
+// -----------JE CREER UNE BALISE P
+let p = document.createElement("p");
+// -----------J'INSERE MA BALISE P DANS MA DIV
+transcription_element.appendChild(p);
 /*
  -----------JE FAIT UN ADDEVENTLISTENER SUR recognition  QUI SE DECLANCHE AVEC L'EVENEMENT RESULT 
  (Se déclenche lorsque le service de reconnaissance vocale renvoie un résultat 
@@ -19,38 +20,38 @@ transcription_element.appendChild(p)
  Aussi disponible via la onresultpropriété.)
 
 */
-recognition.addEventListener('result', (e) => {
-    // -----------JE VAIS RECUPERER MA TRANSCRIPTION 
-    const transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join("")
-    // -----------SI IL CAPTE LE MOT COUCOU IL SE METTRA EN ROUTE (A NOTER QUE JE LE MET EN MODE ECOUTE D'ENTRER)
-    if (transcript.startsWith('coucou')) {
-        // DES QU'IL A LE FEUX VERT J'INSERE MON TEXTE DANS LE MON P 
-        p.textContent = transcript
-        // -----------SI isFinal ME RETOURNE TRUE JE CREER UN AUTRE P PUIS JE LUI INSERE MA TRANSCRIPTION PUIS J'EFFACE MON P 
-        if (e.results[0].isFinal) {
-            p = document.createElement('p')
-            p.textContent = transcript
-            transcription_element.appendChild(p)
-            p.textContent = ''
-            // SI IL CAPTE LE MON SALAM IL ME REPONDRA "wa aleiykoum salam bébé"
-            if (transcript.includes("salam")) {
-                let command = document.createElement('p')
-                command.classList.add('command')
-                command.textContent = "wa aleiykoum salam bébé"
-                transcription_element.appendChild(command)
-            }
-        }
+recognition.addEventListener("result", e => {
+  // -----------JE VAIS RECUPERER MA TRANSCRIPTION
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+  // -----------SI IL CAPTE LE MOT COUCOU IL SE METTRA EN ROUTE (A NOTER QUE JE LE MET EN MODE ECOUTE D'ENTRER)
+  if (transcript.startsWith("coucou")) {
+    // DES QU'IL A LE FEUX VERT J'INSERE MON TEXTE DANS LE MON P
+    p.textContent = transcript;
+    // -----------SI isFinal ME RETOURNE TRUE JE CREER UN AUTRE P PUIS JE LUI INSERE MA TRANSCRIPTION PUIS J'EFFACE MON P
+    if (e.results[0].isFinal) {
+      p = document.createElement("p");
+      p.textContent = transcript;
+      transcription_element.appendChild(p);
+      p.textContent = "";
+      // SI IL CAPTE LE MON 'comment vas tu' IL ME REPONDRA "Je vais bien merci"
+      if (transcript.includes("comment vas tu")) {
+        let command = document.createElement("p");
+        command.classList.add("command");
+        command.textContent = "Je vais bien merci";
+        transcription_element.appendChild(command);
+      }
     }
-})
-// -----------DEMARAGE DE L'PPLICATION 
+  }
+});
+// -----------DEMARAGE DE L'PPLICATION
 
-recognition.addEventListener('end', recognition.start)
-recognition.start()
+recognition.addEventListener("end", recognition.start);
+recognition.start();
 
-// -----------POUR DEMARER L'APPLICATION AU CLICK 
+// -----------POUR DEMARER L'APPLICATION AU CLICK
 
 // btnStart.addEventListener('click',()=>{
 //     btnStop.disabled = false
